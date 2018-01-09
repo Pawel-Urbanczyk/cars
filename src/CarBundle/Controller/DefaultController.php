dca47ac534cbbd4983d049e6235d9b26eb67763f
Paweł Urbańczyk <pawel.urbanczyk90@gmail.com>
2018-01-09 14:39:55 +0100

    Dodanie formularza z wyszukiwaniem, walidajcę i wyświetlanie błędów formularza

--- src/CarBundle/Controller/DefaultController.php
+++ src/CarBundle/Controller/DefaultController.php
@@ -2,19 +2,44 @@
 
 namespace CarBundle\Controller;
 
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/our-cars", name="offer")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $carRespository = $this->getDoctrine()->getRepository('CarBundle:Car');
-        $cars = $carRespository->findCarsWithDetails();
-        return $this->render('CarBundle:Default:index.html.twig', ['cars' => $cars]);
+        $cars           = $carRespository->findCarsWithDetails();
+        $form = $this->createFormBuilder()
+            ->setMethod('GET')
+            ->add('search', \Symfony\Component\Form\Extension\Core\Type\TextType::class, [
+                'constraints' => [
+                        new NotBlank(),
+                        new Length(['min' => 2])
+                    ]
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            die('Form Submited');
+        }
+        return $this->render('CarBundle:Default:index.html.twig',
+            [
+                'cars' => $cars,
+                'form' => $form->createView()
+            ]
+        );
     }
 
     /**
